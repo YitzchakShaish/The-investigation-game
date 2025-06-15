e1cdb00c1d05816247691c5854fe7409ef3175f2
yitzchak shaish <y350380350@gmail.com>
2025-06-15 16:38:13 +0300

    Added metod GetDetectionAccuracy

--- Models/IranianAgents/JuniorAgent.cs
+++ Models/IranianAgents/JuniorAgent.cs
@@ -9,9 +9,9 @@ using The_investigation_game.Interfaces;
 
 namespace The_investigation_game.Models.IranianAgents
 {
-    internal class JuniorAgent: AgentBase
+    internal class JuniorAgent : AgentBase
     {
-        protected override int MaxSecretWeaknesses  { get; set; } = 2;
+        protected override int MaxSecretWeaknesses { get; set; } = 2;
         protected override List<ISensors> SecretWeaknesses { get; set; } = new List<ISensors>();
         protected override List<ISensors> AttachedSensors { get; set; }
 
@@ -34,7 +34,7 @@ namespace The_investigation_game.Models.IranianAgents
 
         public override void AddSecretWeakness(ISensors sensor)
         {
-            if(SecretWeaknesses.Count()<MaxSecretWeaknesses)
+            if (SecretWeaknesses.Count() < MaxSecretWeaknesses)
             {
                 SecretWeaknesses.Add(sensor);
             }
@@ -42,7 +42,20 @@ namespace The_investigation_game.Models.IranianAgents
 
         public override void GetDetectionAccuracy()
         {
-            ;
+            int counter = 0;
+            for (int i = 0; i > MaxSecretWeaknesses; i++)
+            {
+                for (int j = 0; j > MaxSecretWeaknesses; j++)
+                {
+                    if (AttachedSensors[j] == SecretWeaknesses[i])
+                    {
+                        SecretWeaknesses.Remove(SecretWeaknesses[i]);
+                        counter++;
+                        continue;
+                    }
+                }
+            }
+            Console.WriteLine($"Detected agent: {MaxSecretWeaknesses- SecretWeaknesses.Count()} out of {MaxSecretWeaknesses} sensors.");
         }
     }
 }

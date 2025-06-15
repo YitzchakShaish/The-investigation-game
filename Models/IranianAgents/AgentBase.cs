64e5e4267101ab850cc929c4bfdc73571b735881
yitzchak shaish <y350380350@gmail.com>
2025-06-15 15:13:42 +0300

    Added     abstract class AgentBase delete interface Agent

---
+++ Models/IranianAgents/AgentBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using The_investigation_game.Interfaces;
+
+namespace The_investigation_game.Models.IranianAgents
+{
+    abstract class AgentBase
+    {
+        protected abstract List<ISensors> SecretWeaknesses { get; set; }
+        protected abstract List<ISensors> AttachedSensors { get; set; }
+        protected abstract int MaxSecretWeaknesses { get; set; }
+        public abstract void AddSecretWeakness(ISensors sensor);
+        public abstract void AddAttachedSensors(ISensors sensor, int location);
+        public abstract void GetDetectionAccuracy();
+
+
+    }
+}

15f97617355c25ff3002ad8d89c3f4916688bf96
yitzchak shaish <y350380350@gmail.com>
2025-06-16 19:27:58 +0300

    Added class RandomGenerator

---
+++ Services/RandomGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_investigation_game.Services
+{
+    internal static class RandomGenerator
+    {
+        static Random rnd = new Random();
+        public static int GetRandomNumber( int max)
+        {
+            return rnd.Next(0, max);
+        }
+    }
+}

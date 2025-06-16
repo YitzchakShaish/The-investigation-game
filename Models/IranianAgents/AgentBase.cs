6a920c851c6fcdae1308c55d6041530411a2091c
yitzchak shaish <y350380350@gmail.com>
2025-06-16 09:31:39 +0300

    Fixsed fild list SecretWeaknesses to tipe SensorType

--- Models/IranianAgents/AgentBase.cs
+++ Models/IranianAgents/AgentBase.cs
@@ -5,15 +5,16 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using The_investigation_game.Interfaces;
+using The_investigation_game.Models.Sensors;
 
 namespace The_investigation_game.Models.IranianAgents
 {
     abstract class AgentBase
     {
-        protected abstract List<ISensors> SecretWeaknesses { get; set; }
+        protected abstract List<SensorType> SecretWeaknesses { get; set; }
         protected abstract List<ISensors> AttachedSensors { get; set; }
         protected abstract int MaxSecretWeaknesses { get; set; }
-        public abstract void AddSecretWeakness(ISensors sensor);
+        public abstract void AddSecretWeakness(SensorType weakness);
         public abstract void AddAttachedSensors(ISensors sensor, int index);
         public abstract void GetDetectionAccuracy();
 

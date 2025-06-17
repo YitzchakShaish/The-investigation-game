0297888670eb0316c6c05f5134109fe7c96bc2d0
yitzchak shaish <y350380350@gmail.com>
2025-06-17 18:58:30 +0300

    Added class PulseSensor

---
+++ Models/Sensors/PulseSensor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using The_investigation_game.Interfaces;
+
+namespace The_investigation_game.Models.Sensors
+{
+    internal class PulseSensor : ISensors, IBreakableSensor
+    {
+
+        public SensorType Type { get; } = SensorType.Pulse;
+
+        public int MaxActivations { get; } = 3;
+
+        public int CurrentActivations { get; set; } = 0;
+
+        public int ActivationsLeft => MaxActivations - CurrentActivations;
+
+        public bool IsBroken => ActivationsLeft <= 0;
+        public void Activate()
+        {
+            {
+                if (IsBroken)
+                {
+                    Console.WriteLine($"The {Type} sensor  is broken and cannot be activated.");
+                }
+                else
+                {
+                    CurrentActivations++;
+                    Console.WriteLine($"The {Type} sensor  is activated. Activations left: {ActivationsLeft}");
+                }
+            }
+        }
+    }
+
+}

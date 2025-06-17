6f2495facdc359e213bacb9f6baeaacf5a32b5e7
yitzchak shaish <y350380350@gmail.com>
2025-06-17 18:56:10 +0300

    Added class SeniorCommander

---
+++ Models/IranianAgents/SeniorCommander.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using The_investigation_game.Interfaces;
+using The_investigation_game.Services;
+
+namespace The_investigation_game.Models.IranianAgents
+{
+    internal class SeniorCommander : SquadLeader, ICounterattacker
+    {
+        public override int MaxSecretWeaknesses { get;  } = 6;
+
+        public SeniorCommander(string name) : base(name)
+        {
+        }
+        public void Counterattack()
+        {
+            int index = RandomGenerator.GetRandomNumber(AttachedSensors.Count);
+            AttachedSensors.RemoveAt(index);
+            int index2 = RandomGenerator.GetRandomNumber(AttachedSensors.Count);
+            AttachedSensors.RemoveAt(index2);
+        }
+    }
+}

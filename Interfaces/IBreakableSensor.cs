4026724f734ce132285784bb0558f934f3a0e2f5
yitzchak shaish <y350380350@gmail.com>
2025-06-17 18:49:25 +0300

    Added interface IBreakableSensor

---
+++ Interfaces/IBreakableSensor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_investigation_game.Interfaces
+{
+    internal interface IBreakableSensor
+    {
+        int MaxActivations { get; }
+        int CurrentActivations { get; set; }
+        bool IsBroken { get; }
+
+        void Activate();
+    }
+}

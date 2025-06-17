4f1d0e335f1e5726a8a0f44f98ca1d6ca013d580
yitzchak shaish <y350380350@gmail.com>
2025-06-17 19:00:26 +0300

    Added class MagneticSensor

---
+++ Models/Sensors/MagneticSensor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using The_investigation_game.Interfaces;
+using The_investigation_game.Models.IranianAgents;
+
+namespace The_investigation_game.Models.Sensors
+{
+    internal class MagneticSensor : ISensors
+    {
+
+        public SensorType Type { get; } = SensorType.Magnetic;
+
+        public void Activate()
+        {
+            Console.WriteLine($"The {Type} sensor with name  is activated.");
+        }
+        public void PreventCounterattackTwoTimes(ICounterattacker agent)
+        {
+            agent.IncreaseCounterattackThreshold(6);
+        }
+
+    }
+}

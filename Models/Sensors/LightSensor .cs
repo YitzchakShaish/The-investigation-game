93c6e64d67343b4cbf77d9922392319ae529dfe8
yitzchak shaish <y350380350@gmail.com>
2025-06-17 18:58:57 +0300

    Added class LightSensor

---
+++ Models/Sensors/LightSensor .cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using The_investigation_game.Interfaces;
+using The_investigation_game.Models.IranianAgents;
+
+namespace The_investigation_game.Models.Sensors
+{
+    internal class LightSensor : ISensors, IRevealerSensor
+    {
+        
+        public SensorType Type { get; } = SensorType.Light;
+
+        public void Activate()
+        {
+            Console.WriteLine($"The {Type} sensor with name  is activated.");
+        }
+        public string Reveal(JuniorAgent agent)
+        {
+            string name = $"Hey, I found out something about the agent. His name is: {agent.Name}";
+            string organization = $"Hey, I found out something about the agent. He is affiliated with the organization: {agent.TerrorAffiliation.ToString()}";
+            return name + "\n" + organization;
+        }
+    }
+}

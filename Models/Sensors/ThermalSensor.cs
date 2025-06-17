78daf0bd80a46939f63153cdf11276eeb7bc55d4
yitzchak shaish <y350380350@gmail.com>
2025-06-17 18:57:59 +0300

    Fixsed class ThermalSensor

--- Models/Sensors/ThermalSensor.cs
+++ Models/Sensors/ThermalSensor.cs
@@ -4,17 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using The_investigation_game.Interfaces;
+using The_investigation_game.Models.IranianAgents;
 
 namespace The_investigation_game.Models.Sensors
 {
-    internal class ThermalSensor: ISensors
+    internal class ThermalSensor: ISensors, IRevealerSensor
     {
-        public string Name { get; set; }
-        public SensorType Type { get; set; } = SensorType.Thermal;
+       
+        public SensorType Type { get;  } = SensorType.Thermal;
 
         public void Activate()
         {
-            Console.WriteLine($"The {Type} sensor with name '{Name}' is activated.");
+            Console.WriteLine($"The {Type} sensor with name  is activated.");
+        }
+
+        public string Reveal(JuniorAgent agent)
+        {
+            
+
+            return $"Hey, here's one of my weaknesses: \"{agent.GetRandomWeakness()}\"";
         }
     }
 }

eb2dfb0fab68cfa2882b511d51b93d7c80b21a32
yitzchak shaish <y350380350@gmail.com>
2025-06-17 18:50:16 +0300

    Added nterface ICounterattacker

---
+++ Interfaces/ICounterattacker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_investigation_game.Interfaces
+{
+    public interface ICounterattacker
+    {
+        void Counterattack();
+        void IncreaseCounterattackThreshold(int amount);
+    }
+
+}

9dd96c2e803991d8b7de0bddcc13840afa497620
yitzchak shaish <y350380350@gmail.com>
2025-06-18 21:43:37 +0300

    Added method CheckBreakCondition

--- Models/Sensors/PulseSensor.cs
+++ Models/Sensors/PulseSensor.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using The_investigation_game.Interfaces;
+using The_investigation_game.Models.IranianAgents;
 
 namespace The_investigation_game.Models.Sensors
 {
@@ -33,6 +34,17 @@ namespace The_investigation_game.Models.Sensors
                     Console.WriteLine($"The {Type} sensor  is activated. Activations left: {ActivationsLeft}");
                 }
             }
+        }
+
+        public bool CheckBreakCondition()
+        {
+            if (IsBroken)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Pulse sensor broke and removed itself.");
+                Console.ResetColor();
+            }
+            return IsBroken;
         }
     }
 
